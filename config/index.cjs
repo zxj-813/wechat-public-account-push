44b08928fac1d80923843186582333a03280e15e
zxj-813 <54882642+zxj-813@users.noreply.github.com>
2022-10-03 22:49:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -56,7 +56,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oZzpR6ejg766hg66z6GGg9IEkKPU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'cWoCQF4PY4I93TW3eGA_5OOvTwlNnbXUgPy0XCtuvu0',
+      useTemplateId: 'hsGRnZfreFR9XhdEh-dw2Yjljoo4tnIG2JphwpphHFw',
 
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-13',
